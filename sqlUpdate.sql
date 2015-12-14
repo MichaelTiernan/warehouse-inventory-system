838bb7f2273bae7836f2a39921d92b210ab25cc3
antring <emauring@hotmail.com>
2015-12-14 02:05:00 +0100

    Liten bugfux på menyene

--- sqlUpdate.sql
+++ sqlUpdate.sql
@@ -1,3 +1,13 @@
-ALTER TABLE users ADD UNIQUE KEY (`username`);
+ALTER TABLE sales MODIFY `custnr` VARCHAR(10) NOT NULL;
+ALTER TABLE products ADD last_edited_by int(11) unsigned;
 
-ALTER TABLE products MODIFY `ks_storage` int(11) unsigned DEFAULT NULL;
+CREATE TABLE IF NOT EXISTS logg (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  userID int(11) unsigned NOT NULL,
+  quantity int(11) DEFAULT NULL,
+  ks_storage int(11) unsigned DEFAULT NULL,
+  productID int(11) unsigned NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (productID) REFERENCES products(id)
+);
